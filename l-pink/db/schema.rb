70d6264f3fec51518f5a6105c993d093f3a1a929
agarate <alex.garate@usach.cl>
2015-04-22 02:32:29 -0300

    poblado de usuarios y proyectos

--- l-pink/db/schema.rb
+++ l-pink/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419201223) do
+ActiveRecord::Schema.define(version: 20150419201224) do
 
   create_table "acta_reunions", force: :cascade do |t|
     t.date     "fecha"
--- l-pink/db/seeds.rb
+++ l-pink/db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Proyecto.create(:nombre => 'seeds', :descripcion => 'poblado desde seeds', :fecha_inicio => '2015-11-29', :fecha_fin => '2015-11-29')
+Proyecto.create(:nombre => 'L-Pink', :descripcion => 'Proyecto que busca facilitar la metodología scrum, registrando actas dialógicas.', :fecha_inicio => '2015-04-08', :fecha_fin => '2015-04-22')
+Proyecto.create(:nombre => 'Dentool', :descripcion => 'Proyecto centrado en la creación de un software que facilite la obtención de datos estadísticos para atenciones de un centro dental', :fecha_inicio => '2014-07-29', :fecha_fin => '2014-11-29')
+Proyecto.create(:nombre => 'Traminet', :descripcion => 'Proyecto de software que busca crear una plataforma web para compartir información respecto a diferentes tipos de tramites', :fecha_inicio => '2014-07-09', :fecha_fin => '2014-12-03')
