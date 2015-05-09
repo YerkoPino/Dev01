5e5588468a5caf1589450d20bc08c51d8de6e01c
agarate <alex.garate@usach.cl>
2015-05-09 20:18:03 -0300

    agregado registro junto con inicio de sesion

--- l-pink/config/routes.rb
+++ l-pink/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'proyectos/index'
 
-  #get 'users/new'
+  get 'users/new'
 
   resources :usuario_tareas
   resources :usuario_elementos
