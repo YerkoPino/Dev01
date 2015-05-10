c155516014f5bbc0c870ee348f18f0dc6583ba80
agarate <alex.garate@usach.cl>
2015-05-10 01:32:13 -0300

    mail de bienvenida

--- l-pink/config/environments/development.rb
+++ l-pink/config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
    :address => "smtp.gmail.com",
    :port => 587,
    :domain => "gmail.com",
-   :user_name => "lpink@gmail.com",
-   :password => "contrasena",
+   :user_name => "lpink12015@gmail.com",
+   :password => "proyectolpink",
    :authentication => :plain,
    :enable_starttls_auto => true
 
