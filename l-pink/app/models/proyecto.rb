42780ac4a64ff3cd13abb31e8932b721b4415031
José Ignacio Cortés Tapia <jose.cortes@usach.cl>
2015-05-13 01:52:08 -0300

    Mejorado la vista de los proyectos

--- l-pink/app/models/proyecto.rb
+++ l-pink/app/models/proyecto.rb
@@ -4,14 +4,7 @@ class Proyecto < ActiveRecord::Base
 	validates :fecha_inicio, :presence => {:message => "es campo obligatorio"}
 	validates :fecha_fin, :presence => {:message => "es campo obligatorio"}
 	
-	validate :fecha_fin_mayor_presente,
-     :fecha_fin_mayor_inicio
-
-	def fecha_fin_mayor_presente
-		if !fecha_fin.blank? and fecha_fin < Date.today
-			errors.add(:fecha_fin, "no puede estar en el pasado")
-		end
-	end
+	validate :fecha_fin_mayor_inicio	
 
 	def fecha_fin_mayor_inicio
 		if fecha_fin < fecha_inicio
